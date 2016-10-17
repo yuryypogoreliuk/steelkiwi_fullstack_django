6f688d5f5e36fca57d70584ead35ef4bb993eda9
Yuryy Pogoreliuk <yuryypogoreliuk@gmail.com>
2016-10-17 14:50:14 +0300

    Add tests

--- product/tests.py
+++ product/tests.py
@@ -1,4 +1,24 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 from django.test import Client
 
+from .models import Category
+
 client = Client()
+
+class IndexTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_latest_products(self):
+
+        response = self.client.get('/products/')
+        self.assert_(response.status_code in [200, 301, 302])
+        categories_count = Category.objects.count()
+        print response.context
+        #print response.context['object_list']
+        #self.assertEqual(response.context[''])
+
+        
